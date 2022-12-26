3383120f846f1e063d04d042421270633f737f79
anas-reveation <anas.khan@reveation.io>
2022-12-26 18:36:38 +0530

    Add:error Page added

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="error.statusCode === 404">
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+          <div>
+            <img src="../assets/img/404.png" class="w-100" />
+            <h1 class="fw-bold secondary text-center">Error!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "error", // you can set a custom layout for the error page
+  props: ["error"],
+};
+</script>
